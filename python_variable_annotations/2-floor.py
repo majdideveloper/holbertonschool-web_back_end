790f03256504de56f0ef0a7253ab0b88186e8246
majdi aribi <majdi.developer@gmail.com>
2023-06-15 10:23:59 +0100

    python variable annotation

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated  floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    function that reutrn floor
+    """
+    return int(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated  str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function that reutrn str
+    """
+    return str(n)
