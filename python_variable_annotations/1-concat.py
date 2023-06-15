f30d8cc07b5da1c3e452557c5005bf2b6ad04952
majdi aribi <majdi.developer@gmail.com>
2023-06-15 10:06:28 +0100

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annotated function add that takes a string str1 and
+string str2  as arguments and returns concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function that reutrn the sum of a and b
+    """
+    return str1 + str2
