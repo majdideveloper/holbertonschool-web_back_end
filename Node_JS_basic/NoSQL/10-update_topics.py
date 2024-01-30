eb32195d79212e5630a76654d2ce79712ccdd128
Majdi Aribi <majdi.developer@gmail.com>
2024-01-30 15:07:48 +0100

    change folder

---
+++ Node_JS_basic/NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Python function that changes all topics
+of a school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    mongo_collection will be the pymongo
+    collection object
+    """
+    query = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+    mongo_collection.update_many(query, new_values)
---
+++ Node_JS_basic/NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Python function that lists all
+documents in a collection:
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Return an empty list if no document in the collection
+    """
+    documents = mongo_collection.find()
+    if documents.count() == 0:
+        return []
+    return documents
